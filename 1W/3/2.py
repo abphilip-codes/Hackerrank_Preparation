fe3e8bc4d5700970d42cca88634fbb8415ba857c
allenalvin333 <allenalvin333@gmail.com>
2021-11-07 07:20:39 +0530

    Tower Breakers

---
+++ 1W/3/2.py
@@ -0,0 +1,39 @@
+# https://www.hackerrank.com/challenges/one-week-preparation-kit-tower-breakers-1/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'towerBreakers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#
+
+def towerBreakers(n, m):
+    return 1 if(n%2 and m!=1) else 2
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        m = int(first_multiple_input[1])
+
+        result = towerBreakers(n, m)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
