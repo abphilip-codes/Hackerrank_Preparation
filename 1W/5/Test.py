e3f14e9a465c530f9ab89446a9baab53eba28482
allenalvin333 <allenalvin333@gmail.com>
2021-11-09 11:09:16 +0530

    Pairs

---
+++ 1W/5/Test.py
@@ -0,0 +1,40 @@
+# Mock Test
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'pairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY arr
+#
+
+def pairs(k, arr):
+    return len(set(arr) & set([z+k for z in set(arr)])) 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = pairs(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
