c3168695207e2d671e0753986c4b2eb1eef7ea75
allenalvin333 <allenalvin333@gmail.com>
2021-11-11 07:34:01 +0530

    Tree: Preorder Traversal

--- 1W/7/1.py
+++ 1W/7/1.py
@@ -42,6 +42,7 @@ self.left (the left child of the node)
 self.right (the right child of the node)
 self.info (the value of the node)
 """
+
 def preOrder(root):
      if root!=None:
         print(root.info,end=" ")
