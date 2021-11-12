18af5b7471f6002c32101559e5326e92578d9acc
allenalvin333 <allenalvin333@gmail.com>
2021-11-12 06:55:58 +0530

    Diagonal Difference

---
+++ 1M/W1/7.py
@@ -0,0 +1,36 @@
+# https://www.hackerrank.com/challenges/one-month-preparation-kit-diagonal-difference/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr): 
+    return abs(sum([arr[x][x] for x in range(len(arr))])-
+               sum([arr[x][n-1-x] for x in range(len(arr))]))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
