8a110beeeea00e7e51a2e8dd50002f528e0dbdae
allenalvin333 <allenalvin333@gmail.com>
2021-11-12 07:04:46 +0530

    Lonely Integer

---
+++ 1M/W1/5.py
@@ -0,0 +1,32 @@
+# https://www.hackerrank.com/challenges/one-month-preparation-kit-lonely-integer/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    return 2*sum(set(a))-sum(a)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
