6ad9b88fee9cda6f58ba6d367bdf60bdd24a2714
allenalvin333 <allenalvin333@gmail.com>
2021-11-12 07:14:52 +0530

    Flipping bits

--- 1M/W1/6.py
+++ 1M/W1/6.py
@@ -1,2 +1,33 @@
 # https://www.hackerrank.com/challenges/one-month-preparation-kit-flipping-bits/problem
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'flippingBits' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def flippingBits(n):
+    return 2**32-n-1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        result = flippingBits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
