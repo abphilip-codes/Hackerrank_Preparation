8af659ff0807396c472ea92248671415eb5c7d12
allenalvin333 <allenalvin333@gmail.com>
2021-11-13 08:27:32 +0530

    Sum vs XOR

---
+++ 1M/W2/13.py
@@ -0,0 +1,30 @@
+# https://www.hackerrank.com/challenges/one-month-preparation-kit-sum-vs-xor/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sumXor' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def sumXor(n):
+    return 1 if(n==0) else 2**(bin(n)[2:].count('0'))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = sumXor(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
