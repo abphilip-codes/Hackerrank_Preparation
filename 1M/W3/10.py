76084c3d9aac171bce5c73ab754641b597228ce6
allenalvin333 <allenalvin333@gmail.com>
2021-11-15 08:33:46 +0530

    Queue using Two Stacks

--- 1M/W3/10.py
+++ 1M/W3/10.py
@@ -1,2 +1,10 @@
 # https://www.hackerrank.com/challenges/one-month-preparation-kit-queue-using-two-stacks/problem
 
+a,b = [],[]
+for _ in range(int(input())):
+    z = list(map(int,input().split()))
+    if(z[0]==1): b.append(z[1])
+    elif(z[0]==2):
+        if not a: a,b=b[::-1],[]
+        a.pop()
+    else: print(a[-1] if a else b[0])
