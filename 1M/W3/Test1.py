3778cf74c52e2c804edd823dba4b0bd86c21cc98
allenalvin333 <allenalvin333@gmail.com>
2021-11-15 08:36:14 +0530

    Truck Tour

---
+++ 1M/W3/Test1.py
@@ -0,0 +1,38 @@
+# Mock Test I
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'truckTour' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY petrolpumps as parameter.
+#
+
+def truckTour(p):
+    f=a=z=0
+    while(z<len(p)):
+        f+=(p[z][0]-p[z][1])
+        if(f<0): f,a,z=0,a+1,a+1
+        else: z+=1
+    return a
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    petrolpumps = []
+
+    for _ in range(n):
+        petrolpumps.append(list(map(int, input().rstrip().split())))
+
+    result = truckTour(petrolpumps)
