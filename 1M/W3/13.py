8aafc2ff0519d941c5a078924c3881d224de3749
allenalvin333 <allenalvin333@gmail.com>
2021-11-15 08:07:12 +0530

    Simple Text Editor

---
+++ 1M/W3/13.py
@@ -0,0 +1,13 @@
+# https://www.hackerrank.com/challenges/one-month-preparation-kit-simple-text-editor/problem
+
+s,a = '',[]
+for _ in range(int(input())):
+    z = input().split()
+    if z[0]=='1':
+        a.append(s)
+        s = s+z[1]
+    elif z[0]=='2':
+        a.append(s)
+        s = s[0:int(z[1])*-1]
+    elif z[0]=='3': print(s[int(z[1])-1])
+    else: s = a.pop()
