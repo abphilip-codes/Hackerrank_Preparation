60420a1aa5ab5be508120041e9b0342048be68f9
allenalvin333 <allenalvin333@gmail.com>
2021-11-17 08:59:07 +0530

    Equal Stacks

--- 1M/W4/1.py
+++ 1M/W4/1.py
@@ -1,2 +1,52 @@
 # https://www.hackerrank.com/challenges/one-month-preparation-kit-equal-stacks/problem
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'equalStacks' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h1
+#  2. INTEGER_ARRAY h2
+#  3. INTEGER_ARRAY h3
+#
+
+def equalStacks(h1, h2, h3):
+    a,b,c = map(sum,(h1,h2,h3))
+    while h1 and h2 and h3:
+        m = min(a,b,c)
+        while(a>m): a-=h1.pop(0)
+        while(b>m): b-=h2.pop(0)
+        while(c>m): c-=h3.pop(0)
+        if(a==b==c): return a
+    return 0
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n1 = int(first_multiple_input[0])
+
+    n2 = int(first_multiple_input[1])
+
+    n3 = int(first_multiple_input[2])
+
+    h1 = list(map(int, input().rstrip().split()))
+
+    h2 = list(map(int, input().rstrip().split()))
+
+    h3 = list(map(int, input().rstrip().split()))
+
+    result = equalStacks(h1, h2, h3)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
