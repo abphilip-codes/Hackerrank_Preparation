50ab581581697c2a484ba23adf8d2aee90dce865
allenalvin333 <allenalvin333@gmail.com>
2021-11-18 19:25:06 +0530

    Divisible Sum Pairs

---
+++ 3M/W1/6.py
@@ -0,0 +1,39 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-divisible-sum-pairs/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'divisibleSumPairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY ar
+#
+
+def divisibleSumPairs(n, k, ar):
+    return sum((z+y)%k==0 for x,y in enumerate(ar) for z in ar[x+1:])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
