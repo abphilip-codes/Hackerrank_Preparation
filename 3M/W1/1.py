ed4664735f9cb77df14f02794349c82462311aa4
allenalvin333 <allenalvin333@gmail.com>
2021-11-18 19:20:41 +0530

    Plus Minus

---
+++ 3M/W1/1.py
@@ -0,0 +1,19 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-plus-minus/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def plusMinus(a):
+    print(format(len([x for x in a if x > 0])/n, ".6f"))
+    print(format(len([x for x in a if x < 0])/n, ".6f"))
+    print(format(len([x for x in a if x == 0])/n, ".6f"))
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().rstrip().split()))
+    plusMinus(arr)
