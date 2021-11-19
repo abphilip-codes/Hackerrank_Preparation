313742d20e4b3077273b5b0673a07a888713b3fe
allenalvin333 <allenalvin333@gmail.com>
2021-11-19 11:57:48 +0530

    Flipping the Matrix

---
+++ 3M/W2/Test.py
@@ -0,0 +1,44 @@
+# Mock Test
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'flippingMatrix' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY matrix as parameter.
+#
+
+def flippingMatrix(m, ans=0):
+    n = len(m)
+    for z in range(n//2):
+        for y in range(n//2):
+            ans += max(m[z][y], m[z][n-y-1], m[n-z-1][y], m[n-z-1][n-y-1])
+    return ans
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        matrix = []
+
+        for _ in range(2 * n):
+            matrix.append(list(map(int, input().rstrip().split())))
+
+        result = flippingMatrix(matrix)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
