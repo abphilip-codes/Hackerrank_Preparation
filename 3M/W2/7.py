631f616e0482e01640d53b608b6fb702279adb14
allenalvin333 <allenalvin333@gmail.com>
2021-11-19 11:54:16 +0530

    Pangrams

---
+++ 3M/W2/7.py
@@ -0,0 +1,30 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-pangrams/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    return "pangram" if(len(set(s.lower()))==27) else "not pangram"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
