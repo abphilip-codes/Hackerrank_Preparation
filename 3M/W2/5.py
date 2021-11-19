d1f2a184deba1ee3faae3377bf2e5ce7f2500774
allenalvin333 <allenalvin333@gmail.com>
2021-11-19 11:51:06 +0530

    Counting Sort 1

---
+++ 3M/W2/5.py
@@ -0,0 +1,33 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-countingsort1/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    return [arr.count(z) for z in range(100)]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
