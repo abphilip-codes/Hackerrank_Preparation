52ca904f4cf1ccd8ee13ae1f690fd98bfa47437b
allenalvin333 <allenalvin333@gmail.com>
2021-11-19 11:52:53 +0530

    Counting Valleys

---
+++ 3M/W2/6.py
@@ -0,0 +1,41 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-counting-valleys/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    alt = 0
+    valley = 0
+    for z in path:
+        if(z=='U'):
+            alt+=1
+            if(alt==0): valley+=1
+        else: alt-=1
+    return valley
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
