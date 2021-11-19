7fd5a2ee37ffa5e2606f0007033edae59f2e536b
allenalvin333 <allenalvin333@gmail.com>
2021-11-19 11:55:28 +0530

    Mars Exploration

---
+++ 3M/W2/8.py
@@ -0,0 +1,30 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-mars-exploration/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marsExploration' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def marsExploration(s):
+    return sum(s[z]!="SOS"[z%3] for z in range(len(s)))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = marsExploration(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
