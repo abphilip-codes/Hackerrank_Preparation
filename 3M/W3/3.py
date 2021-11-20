cc33e7d9e4a86fe061ec8d3c79f110b5dcefd2b6
allenalvin333 <allenalvin333@gmail.com>
2021-11-20 11:25:16 +0530

    XOR Strings 3

---
+++ 3M/W3/3.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-strings-xor/problem
+
+def strings_xor(s, t):
+    res = ""
+    for i in range(len(s)):
+        if s[i] == t[i]: res += '0';
+        else: res += '1';
+    return res
+
+s = input()
+t = input()
+print(strings_xor(s, t))
