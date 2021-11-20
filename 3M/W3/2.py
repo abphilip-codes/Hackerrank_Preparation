f163abbc777e69b0f74fcfe1164bdcd1cb868077
allenalvin333 <allenalvin333@gmail.com>
2021-11-20 09:17:50 +0530

    Subarray Division 2

---
+++ 3M/W3/2.py
@@ -0,0 +1,41 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-the-birthday-bar/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthday' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY s
+#  2. INTEGER d
+#  3. INTEGER m
+#
+
+def birthday(s, d, m):
+    return len([1 for z in range(len(s)-m+1) if(sum(s[z:z+m])==d)])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    first_multiple_input = input().rstrip().split()
+
+    d = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = birthday(s, d, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
