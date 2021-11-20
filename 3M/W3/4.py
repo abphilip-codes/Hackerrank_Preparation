fadace84332856def0db8f61a6a6badee427400a
allenalvin333 <allenalvin333@gmail.com>
2021-11-20 11:26:08 +0530

    Sales by Match

---
+++ 3M/W3/4.py
@@ -0,0 +1,34 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-sock-merchant/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    return sum([ar.count(z)//2 for z in set(ar)])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
