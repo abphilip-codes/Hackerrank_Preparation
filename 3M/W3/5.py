56c39308d2adc7f174608aa6a6975fdd9099b8af
allenalvin333 <allenalvin333@gmail.com>
2021-11-20 11:27:26 +0530

    Migratory Birds

---
+++ 3M/W3/5.py
@@ -0,0 +1,34 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-migratory-birds/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    l = [0]*5
+    for z in arr: l[z-1]+=1
+    return l.index(max(l))+1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
