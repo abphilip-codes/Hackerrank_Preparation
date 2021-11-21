eefdce9f64cfbbcd2d79ea111204851a95bd9c7a
allenalvin333 <allenalvin333@gmail.com>
2021-11-21 08:01:59 +0530

    Number Line Jumps

---
+++ 3M/W4/3.py
@@ -0,0 +1,45 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-kangaroo/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'kangaroo' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x1
+#  2. INTEGER v1
+#  3. INTEGER x2
+#  4. INTEGER v2
+#
+
+def kangaroo(x1, v1, x2, v2):
+    if(v2==v1): return 'NO'
+    else:
+        z = (x1-x2)/(v2-v1)
+        return 'YES' if(z>=0 and z.is_integer()) else 'NO'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
