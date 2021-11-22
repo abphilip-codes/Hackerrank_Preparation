02648ec4e45638844c40e4b18bfbc6832af65c40
allenalvin333 <allenalvin333@gmail.com>
2021-11-22 08:06:56 +0530

    Max Min

---
+++ 3M/W5/1.py
@@ -0,0 +1,40 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-angry-children/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maxMin' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY arr
+#
+
+def maxMin(k, arr):
+    return min(arr[z+k]-arr[z] for z in range(len(arr)-k))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    k = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = int(input().strip())
+        arr.append(arr_item)
+
+    result = maxMin(k-1, sorted(arr))
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
