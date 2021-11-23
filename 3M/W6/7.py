475371e86ad2c789ea0bafc0846d71cbb7b37348
allenalvin333 <allenalvin333@gmail.com>
2021-11-23 16:52:10 +0530

    Counter game

---
+++ 3M/W6/7.py
@@ -0,0 +1,37 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-counter-game/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'counterGame' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def counterGame(n, z=1):
+    while(n>1):
+        z,k = z+1,0
+        while(2**k<n): k+=1
+        n = n//2 if(2**k==n) else n-2**(k-1)
+    return "Louise" if z % 2 == 0 else "Richard" 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = counterGame(n)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
