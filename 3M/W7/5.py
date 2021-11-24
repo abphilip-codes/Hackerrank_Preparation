1ad2c52bfd5b24c1387e891655c28859fb46310e
allenalvin333 <allenalvin333@gmail.com>
2021-11-24 07:00:34 +0530

    Climbing the Leaderboard

---
+++ 3M/W7/5.py
@@ -0,0 +1,41 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-climbing-the-leaderboard/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from bisect import bisect_right
+
+#
+# Complete the 'climbingLeaderboard' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY ranked
+#  2. INTEGER_ARRAY player
+#
+
+def climbingLeaderboard(ranked, player):
+    r=sorted(list(set(ranked)))
+    return [len(r)-bisect_right(r,z)+1 for z in player]    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ranked_count = int(input().strip())
+
+    ranked = list(map(int, input().rstrip().split()))
+
+    player_count = int(input().strip())
+
+    player = list(map(int, input().rstrip().split()))
+
+    result = climbingLeaderboard(ranked, player)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
