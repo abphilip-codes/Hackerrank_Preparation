27ad963bd5f8091c0e6c4bb8c4210650c51b2fe0
allenalvin333 <allenalvin333@gmail.com>
2021-11-24 07:05:11 +0530

    Insert a node at a specific position in a linked list

---
+++ 3M/W7/8.py
@@ -0,0 +1,60 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-insert-a-node-at-a-specific-position-in-a-linked-list/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+        node = node.next
+        if node:
+            fptr.write(sep)
+
+def insertNodeAtPosition(head, data, position):
+    prev=head
+    if(prev==None): return head
+    n=SinglyLinkedListNode(data)
+    for i in range(0,position-1):
+        if(prev.next==None): return head
+        prev=prev.next
+    n.next=prev.next
+    prev.next=n
+    return head
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    llist_count = int(input())
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+    data = int(input())
+    position = int(input())
+    llist_head = insertNodeAtPosition(llist.head, data, position)
+    print_singly_linked_list(llist_head, ' ', fptr)
+    fptr.write('\n')
+    fptr.close()
