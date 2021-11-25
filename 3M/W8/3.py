ad3c4e96aa708a23d786d13f83292e9e77579cc7
allenalvin333 <allenalvin333@gmail.com>
2021-11-25 06:54:40 +0530

    Ice Cream Parlor

---
+++ 3M/W8/3.py
@@ -0,0 +1,43 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-icecream-parlor/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'icecreamParlor' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER m
+#  2. INTEGER_ARRAY arr
+#
+
+def icecreamParlor(m, arr):
+    s={}
+    for z in range(len(arr)):
+        if(m-arr[z] in s): return [s[m-arr[z]],z+1]
+        else: s[arr[z]]=z+1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        m = int(input().strip())
+
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = icecreamParlor(m, arr)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
