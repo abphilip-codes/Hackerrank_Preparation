f499eaa1af082f90283a3ab674682786484c5ed3
allenalvin333 <allenalvin333@gmail.com>
2021-11-27 09:01:20 +0530

    Minimum Operations 4

--- 3M/W10/8.py
+++ 3M/W10/8.py
@@ -1,2 +1,36 @@
 # https://www.hackerrank.com/challenges/three-month-preparation-kit-minimum-operations/problem
 
+def min_operations(red, green, blue):
+    dp = [[(1<<30) for x in range(8)] for y in range(len(red)+1)]
+    n = len(red)
+    dp[0][0] = 0
+    for i in range(0, n):
+        for j in range(0, 8):
+            dp[i + 1][j | 1] = min(dp[i + 1][j | 1], dp[i][j] + green[i] + blue[i])
+            dp[i + 1][j | 2] = min(dp[i + 1][j | 2], dp[i][j] + red[i] + blue[i])
+            dp[i + 1][j | 4] = min(dp[i + 1][j | 4], dp[i][j] + red[i] + green[i])
+         
+    j = 0
+    for i in range(0, n):
+        if red[i]:
+            j |= 1
+        if green[i]:
+            j |= 2
+        if blue[i]:
+            j |= 4
+
+    if dp[n][j] >= (1<<30):
+        dp[n][j] = -1
+
+    return dp[n][j]
+
+n = int(input())
+red = []
+green = []
+blue = []
+for i in range(n):
+    r, g, b = map(int, input().split())
+    red.append(r)
+    green.append(g)
+    blue.append(b)
+print(min_operations(red, green, blue))
