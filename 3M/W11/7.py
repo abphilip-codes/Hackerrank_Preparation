9069c5fc5b6aa24f359efeae4afd63ed945a055b
allenalvin333 <allenalvin333@gmail.com>
2021-11-28 08:02:20 +0530

    Tree: Preorder Traversal

---
+++ 3M/W11/7.py
@@ -0,0 +1,61 @@
+# https://www.hackerrank.com/challenges/three-month-preparation-kit-tree-preorder-traversal/problem
+
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+
+def preOrder(root):
+    n = root
+    if n is None: return
+    print(n.info, end=' ')
+    preOrder(n.left)
+    preOrder(n.right)
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+preOrder(tree.root)
